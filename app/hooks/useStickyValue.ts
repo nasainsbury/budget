e4167fee7f29902cb1bd4e52df7733ac8868ec37
nasainsbury <nasainsbury@gmail.com>
2024-08-16 21:39:09 +0100

    window stuff

--- app/hooks/useStickyValue.ts
+++ app/hooks/useStickyValue.ts
@@ -2,12 +2,14 @@ import React from "react";
 
 function useStickyState<T>(defaultValue: T, key: string) {
   const [value, setValue] = React.useState<T>(() => {
-    const stickyValue = window.localStorage.getItem(key);
+    const stickyValue = window?.localStorage.getItem(key);
     return stickyValue !== null ? (JSON.parse(stickyValue) as T) : defaultValue;
   });
 
   React.useEffect(() => {
-    window.localStorage.setItem(key, JSON.stringify(value));
+    if (window) {
+      window.localStorage.setItem(key, JSON.stringify(value));
+    }
   }, [key, value]);
 
   return [value, setValue] as const;
