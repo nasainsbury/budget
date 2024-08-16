b98d2baf510c732a1380139ef9de3f30964023ab
nasainsbury <nasainsbury@gmail.com>
2024-08-16 20:49:34 +0100

    cool

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,8 +21,12 @@ export default function RootLayout({
         <nav className="bg-black flex justify-between text-white px-12 py-4 items-center">
           <h1 className="text-2xl font-semibold">Financial Calculator</h1>
           <ul className="flex gap-x-12">
-            <li>Monthly Budget</li>
-            <li>Yearly Savings</li>
+            <li>
+              <Link href="/">Monthly Budget</Link>
+            </li>
+            <li>
+              <Link href="/house">Yearly Budget</Link>
+            </li>
           </ul>
         </nav>
         {children}
