98fffe1b02ab71395b66c66c02bce21c164e0c81
nasainsbury <nasainsbury@gmail.com>
2024-08-21 22:41:02 +0100

    cool

--- app/components/MonthlyBudgetTable.tsx
+++ app/components/MonthlyBudgetTable.tsx
@@ -33,117 +33,94 @@ function MonthlyBudgetTable() {
     20
   );
   return (
-    <div className="col-span-9 overflow-hidden shadow ring-1 ring-black ring-opacity-10 sm:rounded-lg max-h-[500px] overflow-y-scroll w-full">
-      <table className="divide-y divide-stone-3s00 w-full">
-        <thead className="sticky top-0 z-10">
-          <tr>
-            <th
-              className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
-              scope="col"
-            >
-              Year
-            </th>
+    <table className="divide-y divide-stone-300 w-full border-stone-600 border rounded-md border-separate">
+      <thead className="sticky top-0 z-10">
+        <tr className="text-stone-200">
+          <th className="py-3.5 pl-4 pr-3 text-left" scope="col">
+            Year
+          </th>
 
-            <th
-              className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-              scope="col"
-            >
-              Income
-            </th>
-            <th
-              className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-              scope="col"
-            >
-              Expenses
-            </th>
-            <th
-              className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-              scope="col"
-            >
-              Debt
-            </th>
-            <th
-              className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-              scope="col"
-            >
-              Remaining Debt
-            </th>
-            <th
-              className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-              scope="col"
-            >
-              Savings
-            </th>
-            <th
-              className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-              scope="col"
-            >
-              Total Savings
-            </th>
-          </tr>
-        </thead>
-        <tbody className="divide-y divide-gray-200">
-          {budget.map((result) => {
-            const expensePercent = (
-              (result.expenses.total / result.income.total) *
-              100
-            ).toPrecision(3);
+          <th className="px-3 py-3.5 text-left" scope="col">
+            Income
+          </th>
+          <th className="px-3 py-3.5 text-left" scope="col">
+            Expenses
+          </th>
+          <th className="px-3 py-3.5 text-left" scope="col">
+            Debt
+          </th>
+          <th className="px-3 py-3.5 text-left" scope="col">
+            Remaining Debt
+          </th>
+          <th className="px-3 py-3.5 text-left" scope="col">
+            Savings
+          </th>
+          <th className="px-3 py-3.5 text-left" scope="col">
+            Total Savings
+          </th>
+        </tr>
+      </thead>
+      <tbody className="divide-y divide-gray-200">
+        {budget.map((result) => {
+          const expensePercent = (
+            (result.expenses.total / result.income.total) *
+            100
+          ).toPrecision(3);
 
-            const debtPercent = (
-              (result.debt.total / result.income.total) *
-              100
-            ).toPrecision(3);
+          const debtPercent = (
+            (result.debt.total / result.income.total) *
+            100
+          ).toPrecision(3);
 
-            const savingsPercent = (
-              (result.savings.total / result.income.total) *
-              100
-            ).toPrecision(3);
-            return (
-              <tr className="hover:bg-stone-500" key={result.date.toString()}>
-                <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
-                  {result.date.toFormat("LLL yyyy")}
-                </td>
-                <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-600">
-                  {formatValue(result.income.total)}
-                </td>
-                <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-600">
+          const savingsPercent = (
+            (result.savings.total / result.income.total) *
+            100
+          ).toPrecision(3);
+          return (
+            <tr className="hover:bg-stone-500" key={result.date.toString()}>
+              <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm text-stone-200 sm:pl-6">
+                {result.date.toFormat("LLL yyyy")}
+              </td>
+              <td className="whitespace-nowrap px-3 py-4 text-sm text-stone-200">
+                {formatValue(result.income.total)}
+              </td>
+              <td className="whitespace-nowrap px-3 py-4 text-sm text-stone-200">
+                <div className="flex gap-x-2 items-end">
+                  <span>{formatValue(result.expenses.total)}</span>
+                  <span className="text-xs text-stone-300">
+                    {expensePercent}%
+                  </span>
+                </div>
+              </td>
+              <td className="whitespace-nowrap px-3 py-4 text-sm text-stone-200">
+                <div className="flex flex-col gap-y-1">
                   <div className="flex gap-x-2 items-end">
-                    <span>{formatValue(result.expenses.total)}</span>
-                    <span className="text-xs text-gray-400">
-                      {expensePercent}%
+                    <span>{formatValue(result.debt.total)}</span>
+                    <span className="text-xs text-stone-300">
+                      {debtPercent}%
                     </span>
                   </div>
-                </td>
-                <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-600">
-                  <div className="flex flex-col gap-y-1">
-                    <div className="flex gap-x-2 items-end">
-                      <span>{formatValue(result.debt.total)}</span>
-                      <span className="text-xs text-gray-400">
-                        {debtPercent}%
-                      </span>
-                    </div>
-                  </div>
-                </td>
-                <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-600 font-bold">
-                  {formatValue(result.debt.totalBalance)}
-                </td>
-                <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-600">
-                  <div className="flex gap-x-2 items-end">
-                    <span>{formatValue(result.savings.total)}</span>
-                    <span className="text-xs text-gray-400">
-                      {savingsPercent}%
-                    </span>
-                  </div>
-                </td>
-                <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-600 font-bold">
-                  {formatValue(result.savings.totalBalance)}
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
+                </div>
+              </td>
+              <td className="whitespace-nowrap px-3 py-4 text-sm text-stone-200 font-bold">
+                {formatValue(result.debt.totalBalance)}
+              </td>
+              <td className="whitespace-nowrap px-3 py-4 text-sm text-stone-200">
+                <div className="flex gap-x-2 items-end">
+                  <span>{formatValue(result.savings.total)}</span>
+                  <span className="text-xs text-stone-300">
+                    {savingsPercent}%
+                  </span>
+                </div>
+              </td>
+              <td className="whitespace-nowrap px-3 py-4 text-sm text-stone-200 font-bold">
+                {formatValue(result.savings.totalBalance)}
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 }
 
